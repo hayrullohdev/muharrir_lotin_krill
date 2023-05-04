552fa9bbcc78b4ad0a88e2c9fb7ba25bd5d5ef72
hayrullohdev <hayrullohabdusamatov@gmail.com>
2023-05-04 18:35:36 +0300

    muharrir_latin_kril_uz

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,27 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:muharrir/lotin_kiril.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => const MyApp(), // Wrap your app
+  ),
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Muharrir UZ',
-      theme: ThemeData(    
-        textTheme: const TextTheme(),   
-        primarySwatch: Colors.blue,
-      ),
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
       home:  const Lotin_KirilPage(),
     );
   }
